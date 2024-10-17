df47e3282a4ab43583135a52b600738ff399dded
Ross King <ross.king@altavec.com>
2024-10-17 14:23:02 +1100

    ✨ add LZMA encoder

---
+++ src/IO.Compression.Lzma/ISetCoderProperties.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ISetCoderProperties.cs" company="Altavec">
+// Copyright (c) Altavec. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Altavec.IO.Compression;
+
+/// <summary>
+/// Interface for setting coder properties.
+/// </summary>
+internal interface ISetCoderProperties
+{
+    /// <summary>
+    /// Sets the coder properties.
+    /// </summary>
+    /// <param name="propIDs">The property IDs.</param>
+    /// <param name="properties">The properties.</param>
+    void SetCoderProperties(CoderPropID[] propIDs, object[] properties);
+}
---
+++ src/IO.Compression.Lzma/IWriteCoderProperties.cs
@@ -0,0 +1,19 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IWriteCoderProperties.cs" company="Altavec">
+// Copyright (c) Altavec. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Altavec.IO.Compression;
+
+/// <summary>
+/// Interface for writing the coder properties.
+/// </summary>
+internal interface IWriteCoderProperties
+{
+    /// <summary>
+    /// Writes the coder properties.
+    /// </summary>
+    /// <param name="outStream">The stream to write to.</param>
+    void WriteCoderProperties(Stream outStream);
+}
